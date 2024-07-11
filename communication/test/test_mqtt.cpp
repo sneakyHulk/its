037b7deff154f0e93150150b756009ea679e4650
Lukas Heyn <lukas.heyn@gmail.com>
2024-07-11 15:55:28 +0200

    mosquitto test

--- communication/test/test_mqtt.cpp
+++ communication/test/test_mqtt.cpp
@@ -17,24 +17,24 @@ using namespace std::chrono_literals;
 class [[maybe_unused]] ImageStreamMQTT : public OutputPtrNode<ImageData> {
 	std::shared_ptr<ImageData const> _image_data;
 	std::string const cam_name;
+	struct mosquitto *mosq;
+
+	static int mosquitto_lib_init_code;
 
    public:
 	explicit ImageStreamMQTT(std::string const &cam_name) : cam_name(cam_name) {
-		mosquitto_lib_init();
+		if (mosquitto_lib_init_code != MOSQ_ERR_SUCCESS) throw common::Exception("[ImageStreamMQTT]: mosquitto_lib cannot be initialized!");
 
-		struct mosquitto *mosq = mosquitto_new("publisher-client", true, NULL);
-		if (!mosq) {
-			throw common::Exception("Error: Out of memory.");
-		}
+		mosq = mosquitto_new(cam_name.c_str(), true, NULL);
+		if (!mosq) throw common::Exception("[ImageStreamMQTT]: ", errno == EINVAL ? "invalid input parameter!" : "out of memory!");
 
-		mosquitto_connect_callback_set(mosq, ImageStreamMQTT::on_connect);
+		mosquitto_int_option(mosq, MOSQ_OPT_PROTOCOL_VERSION, MQTT_PROTOCOL_V5);
+
+		// mosquitto_connect_callback_set(mosq, ImageStreamMQTT::on_connect);
 		mosquitto_publish_callback_set(mosq, ImageStreamMQTT::on_publish);
 
 		int rc = mosquitto_connect(mosq, "localhost", 1883, 60);
-		if (rc != MOSQ_ERR_SUCCESS) {
-			std::cerr << "Could not connect to Broker with return code " << rc << std::endl;
-			throw common::Exception("Could not connect to Broker with return code ", rc, ".");
-		}
+		if (rc != MOSQ_ERR_SUCCESS) throw common::Exception("[ImageStreamMQTT]: ", "could not connect to Broker with return code ", rc, "!");
 
 		mosquitto_loop_start(mosq);
 	}
@@ -43,7 +43,7 @@ class [[maybe_unused]] ImageStreamMQTT : public OutputPtrNode<ImageData> {
 		if (rc == 0) {
 			std::cout << "Connected to broker!" << std::endl;
 			const char *message = "Hello, MQTT!";
-			mosquitto_publish(mosq, NULL, "test/topic", strlen(message), message, 0, false);
+
 		} else {
 			std::cout << "Failed to connect, return code " << rc << std::endl;
 		}
@@ -52,8 +52,16 @@ class [[maybe_unused]] ImageStreamMQTT : public OutputPtrNode<ImageData> {
 	static void on_publish(struct mosquitto *mosq, void *obj, int mid) { std::cout << "Message with mid " << mid << " has been published." << std::endl; }
 
    private:
-	void output_function(std::shared_ptr<ImageData const> const &data) final { std::atomic_store(&_image_data, data); }
+	void output_function(std::shared_ptr<ImageData const> const &data) final {
+		mosquitto_property *proplist = nullptr;
+		mosquitto_property_add_binary(&proplist, 0, &data->timestamp, sizeof(data->timestamp));
+		mosquitto_property_add_string(&proplist, MQTT_PROP_CONTENT_TYPE, &data->timestamp, sizeof(data->timestamp));
+
+		mosquitto_publish_v5(mosq, NULL, "test/topic", , , 0, false);
+	}
 };
+
+int ImageStreamMQTT::mosquitto_lib_init_code = mosquitto_lib_init();
 
 int main(int argc, char **argv) {
 	CameraSimulator cam_n("s110_n_cam_8");
@@ -69,6 +77,5 @@ int main(int argc, char **argv) {
 	std::thread transmitter_thread(&ImageStreamMQTT::operator(), &transmitter);
 	std::thread transmitter2_thread(&ImageStreamMQTT::operator(), &transmitter2);
 
-	for(;;)
-		std::this_thread::sleep_for(10s);
+	for (;;) std::this_thread::sleep_for(10s);
 }
