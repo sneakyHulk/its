3bc95e6cccd9927f8ade8b8520ef3532faf3053d
Lukas Heyn <lukas.heyn@gmail.com>
2024-08-11 17:11:57 +0200

    new shit

--- communication/include/image_communication_node.h
+++ communication/include/image_communication_node.h
@@ -16,7 +16,7 @@ struct StreamContext {
 
 class ImageStreamRTSP : public OutputPtrNode<ImageData> {
 	inline static const bool use_jpeg = false;
-	inline static const char *port = "7000";
+	inline static const char *port = "8554";
 
 	static GstRTSPServer *_server;
 	static GstRTSPMountPoints *_mounts;
