9c3c1ada6ed2afea23b69eac9dc3e9415582600c
Lukas Heyn <lukas.heyn@gmail.com>
2024-08-14 19:42:22 +0200

    cam test

--- camera/test/test_camera.cpp
+++ camera/test/test_camera.cpp
@@ -1,5 +1,9 @@
 #include <pylon/PylonIncludes.h>
 
+#include <ranges>
+#include <vector>
+
+#include "common_exception.h"
 #include "common_output.h"
 
 int main() {
@@ -9,12 +13,20 @@ int main() {
 
 	Pylon::DeviceInfoList device_list;
 	pylon_instance.EnumerateDevices(device_list);
+	if (device_list.empty()) throw common::Exception("No Basler devices found!");
 
-	if (device_list.empty()) common::println("No Basler devices found!");
+	Pylon::CInstantCamera camera(pylon_instance.CreateDevice(device_list.at(0)));
 
-	for (auto const& e : device_list) {
-		common::println(e.GetModelName(), ": ", e.GetIpAddress(), ", ", e.GetMacAddress());
-	}
+	common::println("Using device ", camera.GetDeviceInfo().GetModelName());
+
+
+	// Pylon::CInstantCameraArray cameras(device_list.size());
+	//
+	// for (auto i = 0; i < device_list.size(); ++i) {
+	//	common::println("Found device with model name '", device_list.at(i).GetModelName(), "', ip address '", device_list.at(i).GetIpAddress(), "', and mac address '", device_list.at(i).GetMacAddress(), "'.");
+	//
+	//	cameras[i].Attach(pylon_instance.CreateDevice(device_list.at(i)));
+	//}
 
 	Pylon::PylonTerminate();
 	return 0;
