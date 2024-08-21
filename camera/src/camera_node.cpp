8765260c434a6a2607dff007565b2e646d0eba39
Lukas Heyn <lukas.heyn@gmail.com>
2024-08-21 14:51:30 +0200

    ptp test

--- camera/src/camera_node.cpp
+++ camera/src/camera_node.cpp
@@ -83,7 +83,7 @@ void Camera::init_camera() {
 					clock_offsets.push_back(current_offset);
 
 					common::println("[Camera]: Offset from master approx. ", current_offset, ", max offset is ", *std::max_element(clock_offsets.begin(), clock_offsets.end()));
-					std::this_thread::sleep_for(1ms);
+					std::this_thread::sleep_for(100ms);
 				} while (*std::max_element(clock_offsets.begin(), clock_offsets.end()) > 1ms);
 
 				// common::println("[Camera]: Waiting for slave mode...");
@@ -93,7 +93,7 @@ void Camera::init_camera() {
 			} else {
 				common::println("[Camera]: Camera in monitor mode.");
 
-				std::this_thread::sleep_for(10s);
+				std::this_thread::sleep_for(20s);
 
 				// The default configuration must be removed when monitor mode is selected
 				// because the monitoring application is not allowed to modify any parameter settings.
