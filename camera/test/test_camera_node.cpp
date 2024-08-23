119e524bbc177a81cd22ca1cefff12c55e72cb63
Lukas Heyn <lukas.heyn@gmail.com>
2024-08-23 12:29:59 +0200

    multiple camera, multiple access test 15ms

--- camera/test/test_camera_node.cpp
+++ camera/test/test_camera_node.cpp
@@ -17,28 +17,28 @@ int main(int argc, char* argv[]) {
 	{
 		Camera cam_s("0030532A9B7F", "s110_s_cam_8");
 		Camera cam_o("003053305C72", "s110_o_cam_8");
-		// Camera cam_n("003053305C75", "s110_n_cam_8");
-		// Camera cam_w("003053380639", "s110_w_cam_8");
+		Camera cam_n("003053305C75", "s110_n_cam_8");
+		Camera cam_w("003053380639", "s110_w_cam_8");
 
 		VideoVisualization vid_s;
 		VideoVisualization vid_o;
-		// VideoVisualization vid_n;
-		// VideoVisualization vid_w;
+		VideoVisualization vid_n;
+		VideoVisualization vid_w;
 
 		cam_s += vid_s;
 		cam_o += vid_o;
-		// cam_n += vid_n;
-		// cam_w += vid_w;
+		cam_n += vid_n;
+		cam_w += vid_w;
 
 		std::thread cam_s_thread(&Camera::operator(), &cam_s);
 		std::thread cam_o_thread(&Camera::operator(), &cam_o);
-		// std::thread cam_n_thread(&Camera::operator(), &cam_n);
-		// std::thread cam_w_thread(&Camera::operator(), &cam_w);
+		std::thread cam_n_thread(&Camera::operator(), &cam_n);
+		std::thread cam_w_thread(&Camera::operator(), &cam_w);
 
 		std::thread vid_s_thread(&VideoVisualization::operator(), &vid_s);
 		std::thread vid_o_thread(&VideoVisualization::operator(), &vid_o);
-		// std::thread vid_n_thread(&VideoVisualization::operator(), &vid_n);
-		// std::thread vid_w_thread(&VideoVisualization::operator(), &vid_w);
+		std::thread vid_n_thread(&VideoVisualization::operator(), &vid_n);
+		std::thread vid_w_thread(&VideoVisualization::operator(), &vid_w);
 
 		std::this_thread::sleep_for(100000s);
 	}
