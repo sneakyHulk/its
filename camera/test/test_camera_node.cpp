bdd6c9dba103574e6134208c6162a62e5476ac59
Lukas Heyn <lukas.heyn@gmail.com>
2024-08-26 13:32:33 +0200

    multiple camera, multiple access test 15ms

--- camera/test/test_camera_node.cpp
+++ camera/test/test_camera_node.cpp
@@ -15,6 +15,36 @@ int main(int argc, char* argv[]) {
 	PylonRAII pylon_raii;
 
 	{
+		Pylon::DeviceInfoList device_list;
+		Pylon::CTlFactory::GetInstance().EnumerateDevices(device_list);
+		if (device_list.empty()) throw common::Exception("No Basler devices found!");
+
+		for (auto i = 0; i < device_list.size(); ++i) {
+			common::println("Found ", i, " device with model name '", device_list.at(i).GetModelName(), "', ip address '", device_list.at(i).GetIpAddress(), "', and mac address '", device_list.at(i).GetMacAddress(), "'.");
+
+			Pylon::CBaslerUniversalInstantCamera camera;
+			camera.Attach(Pylon::CTlFactory::GetInstance().CreateDevice(device_list.at(i)));
+
+			camera.Open();
+			camera.StartGrabbing();
+
+			Pylon::CGrabResultPtr ptrGrabResult;
+			camera.RetrieveResult(1000, ptrGrabResult);
+
+			cv::Mat bayer_image(static_cast<int>(ptrGrabResult->GetHeight()), static_cast<int>(ptrGrabResult->GetWidth()), CV_8UC1, ptrGrabResult->GetBuffer());
+			cv::Mat image;
+			cv::cvtColor(bayer_image, image, cv::COLOR_BayerBG2BGR);
+
+			std::string time(std::to_string(ptrGrabResult->GetTimeStamp()));
+
+			if (cv::imwrite(std::filesystem::path(CMAKE_SOURCE_DIR) / std::filesystem::path(std::string("result/img_") + std::to_string(i) + ".png"), image)) {
+			} else {
+				common::println("Problem with imwrite!");
+			}
+		}
+	}
+
+	{
 		Camera cam_s("0030532A9B7F", "s110_s_cam_8");
 		Camera cam_o("003053305C72", "s110_o_cam_8");
 		Camera cam_n("003053305C75", "s110_n_cam_8");
