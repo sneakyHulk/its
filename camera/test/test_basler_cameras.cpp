303fa7560636303282b6520c5200b34da45480a0
Lukas Heyn <lukas.heyn@gmail.com>
2024-11-04 18:49:54 +0100

    test ecal

--- camera/test/test_basler_cameras.cpp
+++ camera/test/test_basler_cameras.cpp
@@ -1,7 +1,6 @@
 #include <stdexcept>
 
 #include "BaslerCamerasNode.h"
-#include "EcalImageDriverNode.h"
 #include "ImageVisualizationNode.h"
 #include "PreprocessingNode.h"
 #include "common_exception.h"
@@ -21,21 +20,25 @@ int main(int argc, char* argv[]) {
 		Pylon::PylonInitialize();
 		common::println("initialzed!");
 
-		common::print("Initialize eCAL...");
-		eCAL::Initialize(argc, argv, "ITS");
-		common::println("initialzed!");
+		Pylon::DeviceInfoList_t device_list;
+		Pylon::CTlFactory::GetInstance().EnumerateDevices(device_list);
 
-		BaslerCamerasNode cameras({{"s110_s_cam_8", {"0030532A9B7F"}}, {"s110_o_cam_8", {"003053305C72"}}, {"s110_n_cam_8", {"003053305C75"}}, {"s110_w_cam_8", {"003053380639"}}});
-		PreprocessingNode pre({{"s110_n_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}, {"s110_w_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}},
-		    {"s110_s_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}, {"s110_o_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}});
-		ImageVisualizationNode img([](ImageData const& data) { return data.source == "s110_s_cam_8"; });
+		for (auto const& device : device_list) {
+			common::println("[BaslerCamerasNode]: Found device ", "name", " with model name '", device.GetModelName(), "', ip address '", device.GetIpAddress(), "', and mac address '", device.GetMacAddress(), "'.");
+		}
 
-		cameras += pre;
-		pre += img;
+		// BaslerCamerasNode cameras({{"s110_s_cam_8", {"0030532A9B7F"}}, {"s110_o_cam_8", {"003053305C72"}}, {"s110_n_cam_8", {"003053305C75"}}, {"s110_w_cam_8", {"003053380639"}}});
+		// BaslerCamerasNode cameras({{"s060_s_cam_16_k_south", {"00305338063B"}}, {"s060_s_cam_16_k_north", {"0030532A9B7D"}}});
+		// PreprocessingNode pre({{"s110_n_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}, {"s110_w_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}},
+		//     {"s110_s_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}, {"s110_o_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}});
+		// ImageVisualizationNode img([](ImageData const& data) { return data.source == "s110_s_cam_8"; });
+		//
+		// cameras += pre;
+		// pre += img;
 
-		std::thread cameras_thread(&BaslerCamerasNode::operator(), &cameras);
-		std::thread pre_thread(&PreprocessingNode::operator(), &pre);
-		std::thread img_thread(&ImageVisualizationNode::operator(), &img);
+		// std::thread cameras_thread(&BaslerCamerasNode::operator(), &cameras);
+		// std::thread pre_thread(&PreprocessingNode::operator(), &pre);
+		// std::thread img_thread(&ImageVisualizationNode::operator(), &img);
 		std::this_thread::sleep_for(10s);
 	} catch (...) {
 		common::print("Terminate Pylon...");
@@ -44,4 +47,8 @@ int main(int argc, char* argv[]) {
 
 		rethrow_exception(std::current_exception());
 	}
+
+	common::print("Terminate Pylon...");
+	Pylon::PylonTerminate();
+	common::println("terminated!");
 }
