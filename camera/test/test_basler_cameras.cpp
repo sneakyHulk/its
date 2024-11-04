b6ff6ccfd5bd73213e3dbb5369dd3586af583e59
Lukas Heyn <lukas.heyn@gmail.com>
2024-11-04 19:11:47 +0100

    test ecal

--- camera/test/test_basler_cameras.cpp
+++ camera/test/test_basler_cameras.cpp
@@ -27,12 +27,18 @@ int main(int argc, char* argv[]) {
 			throw common::Exception("[BaslerCamerasNode]: No Basler camera devices found!");
 		}
 
-		for (auto const& device : device_list) {
+		Pylon::CInstantCameraArray cameras(device_list.size());
+
+		for (auto i = 0; auto& device : device_list) {
 			common::println("[BaslerCamerasNode]: Found device ", "name", " with model name '", device.GetModelName(), "', ip address '", device.GetIpAddress(), "', and mac address '", device.GetMacAddress(), "'.");
+
+			cameras[i++].Attach(Pylon::CTlFactory::GetInstance().CreateDevice(device));
+
+			common::println("Using device ", cameras[i].GetDeviceInfo().GetModelName());
 		}
 
 		// BaslerCamerasNode cameras({{"s110_s_cam_8", {"0030532A9B7F"}}, {"s110_o_cam_8", {"003053305C72"}}, {"s110_n_cam_8", {"003053305C75"}}, {"s110_w_cam_8", {"003053380639"}}});
-		BaslerCamerasNode cameras({{"s060_s_cam_16_k_south", {"00305338063B"}}, {"s060_s_cam_16_k_north", {"0030532A9B7D"}}});
+		// BaslerCamerasNode cameras({{"s060_s_cam_16_k_south", {"00305338063B"}}, {"s060_s_cam_16_k_north", {"0030532A9B7D"}}});
 		// PreprocessingNode pre({{"s110_n_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}, {"s110_w_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}},
 		//     {"s110_s_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}, {"s110_o_cam_8", {1200, 1920, cv::ColorConversionCodes::COLOR_BayerBG2BGR}}});
 		// ImageVisualizationNode img([](ImageData const& data) { return data.source == "s110_s_cam_8"; });
