42960b300f7d86a88a2bf4c0fae9a264fec0754a
Lukas Heyn <lukas.heyn@gmail.com>
2024-11-06 15:48:21 +0100

    test fps

--- camera/include/BaslerCamerasNode.h
+++ camera/include/BaslerCamerasNode.h
@@ -100,7 +100,7 @@ class BaslerCamerasNode : public Pusher<ImageDataRaw> {
 
 			common::print("[BaslerCamerasNode]: Starting grabbing...");
 
-			_cameras.StartGrabbing(Pylon::EGrabStrategy::GrabStrategy_UpcomingImage);
+			_cameras.StartGrabbing(Pylon::EGrabStrategy::GrabStrategy_LatestImageOnly);
 
 			common::println("done!");
 		} catch (Pylon::GenericException const& e) {
