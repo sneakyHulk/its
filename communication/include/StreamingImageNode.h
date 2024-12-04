ded2c9ba6848f93e5c49b4a813f7ec49f835d5a3
Lukas Heyn <lukas.heyn@gmail.com>
2024-12-04 13:37:54 +0100

    common update

--- communication/include/StreamingImageNode.h
+++ communication/include/StreamingImageNode.h
@@ -38,8 +38,6 @@ class StreamingImageNode : public Runner<ImageData> {
 
    public:
 	StreamingImageNode() {
-		common::println("\033[4;31mbold red text\033[0m\n", "test", common::LOGLEVEL::ERROR);
-
 		// Create an RTSP server only once
 		if (!server) {
 			server = gst_rtsp_server_new();
@@ -63,11 +61,11 @@ class StreamingImageNode : public Runner<ImageData> {
 			loop_thread = std::thread([]() {
 				/* attach the server to the default maincontext */
 				if (!gst_rtsp_server_attach(server, NULL)) {
-					throw common::Exception("Failed to attach the RTSP server");
+					common::println_critical_loc("Failed to attach the RTSP server");
 				}
 
 				GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-				common::println("RTSP server is running at rtsp://127.0.0.1:8554/test");
+				common::println_loc("RTSP server is running at rtsp://127.0.0.1:8554/test");
 				g_main_loop_run(loop);
 
 				g_main_loop_unref(loop);
@@ -98,7 +96,7 @@ class StreamingImageNode : public Runner<ImageData> {
 		gst_object_unref(pipeline);
 
 		if (GstState state; gst_element_get_state(source, &state, nullptr, GST_CLOCK_TIME_NONE) != GST_STATE_CHANGE_SUCCESS || state != GST_STATE_PLAYING) {
-			throw common::Exception("Pipeline is not in PLAYING state");
+			common::println_critical_loc("Pipeline is not in PLAYING state");
 		}
 
 		payloader = std::exchange(user_data->payloader, payloader);
@@ -118,9 +116,9 @@ class StreamingImageNode : public Runner<ImageData> {
 
 		// Add a callback to set up appsrc
 		g_signal_connect(factory, "media-configure", G_CALLBACK(media_configure), user_data);
-	}
 
-	guint64 timestamp = 0;
+		run(data);
+	}
 
 	void run(ImageData const &data) final {
 		if (!user_data->playing.test_and_set()) {
@@ -144,13 +142,13 @@ class StreamingImageNode : public Runner<ImageData> {
 			case GST_FLOW_OK:
 				for (GstMessage *msg = gst_bus_pop(user_data->bus); msg;) {
 					switch (GST_MESSAGE_TYPE(msg)) {
-						case GST_MESSAGE_UNKNOWN: common::println("GST_MESSAGE_UNKNOWN: An undefined message.\n"); break;
-						case GST_MESSAGE_EOS: common::println("GST_MESSAGE_EOS: End-of-stream reached. Applications should stop playback when receiving this in the PLAYING state.\n"); break;
+						case GST_MESSAGE_UNKNOWN: common::println_loc("GST_MESSAGE_UNKNOWN: An undefined message."); break;
+						case GST_MESSAGE_EOS: common::println_loc("GST_MESSAGE_EOS: End-of-stream reached. Applications should stop playback when receiving this in the PLAYING state."); break;
 						case GST_MESSAGE_ERROR:
 							GError *err;
 							gchar *err_info;
 							gst_message_parse_error(msg, &err, &err_info);
-							common::println("GST_MESSAGE_ERROR: Error received from element ", GST_OBJECT_NAME(msg->src), ": ", err->message, "\n Debugging information: ", err_info);
+							common::println_error_loc("GST_MESSAGE_ERROR: Error received from element ", GST_OBJECT_NAME(msg->src), ": ", err->message, "\n Debugging information: ", err_info);
 							g_clear_error(&err);
 							g_free(err_info);
 							break;
@@ -158,91 +156,66 @@ class StreamingImageNode : public Runner<ImageData> {
 							GError *warn;
 							gchar *warn_info;
 							gst_message_parse_warning(msg, &warn, &warn_info);
-							common::println("GST_MESSAGE_WARNING: Warning received from element ", GST_OBJECT_NAME(msg->src), ": ", warn->message, "\n Debugging information: ", warn_info);
+							common::println_warn_loc("GST_MESSAGE_WARNING: Warning received from element ", GST_OBJECT_NAME(msg->src), ": ", warn->message, "\n Debugging information: ", warn_info);
 							g_clear_error(&err);
 							g_free(warn_info);
 							break;
-						case GST_MESSAGE_INFO: common::println("GST_MESSAGE_INFO: An informational message.\n"); break;
-						case GST_MESSAGE_TAG: common::println("GST_MESSAGE_TAG: A metadata tag was found.\n"); break;
-						case GST_MESSAGE_BUFFERING: common::println("GST_MESSAGE_BUFFERING: The pipeline is buffering. For non-live pipelines, the application must pause until buffering completes.\n"); break;
+						case GST_MESSAGE_INFO: common::println_loc("GST_MESSAGE_INFO: An informational message."); break;
+						case GST_MESSAGE_TAG: common::println_loc("GST_MESSAGE_TAG: A metadata tag was found."); break;
+						case GST_MESSAGE_BUFFERING: common::println_loc("GST_MESSAGE_BUFFERING: The pipeline is buffering. For non-live pipelines, the application must pause until buffering completes."); break;
 						case GST_MESSAGE_STATE_CHANGED:
-							common::println("GST_MESSAGE_STATE_CHANGED:");
 							GstState old_state, new_state;
 							gst_message_parse_state_changed(msg, &old_state, &new_state, NULL);
-							common::println("GST_MESSAGE_STATE_CHANGED: Element ", GST_OBJECT_NAME(msg->src), " changed state from ", gst_element_state_get_name(old_state), " to ", gst_element_state_get_name(new_state));
+							common::println_loc("GST_MESSAGE_STATE_CHANGED: Element ", GST_OBJECT_NAME(msg->src), " changed state from ", gst_element_state_get_name(old_state), " to ", gst_element_state_get_name(new_state));
 							break;
-						case GST_MESSAGE_STATE_DIRTY: common::println("GST_MESSAGE_STATE_DIRTY: An element changed state in a streaming thread (deprecated).\n"); break;
-						case GST_MESSAGE_STEP_DONE: common::println("GST_MESSAGE_STEP_DONE: A stepping operation finished.\n"); break;
-						case GST_MESSAGE_CLOCK_PROVIDE: common::println("GST_MESSAGE_CLOCK_PROVIDE: An element provides a clock. This is an internal message.\n"); break;
-						case GST_MESSAGE_CLOCK_LOST: common::println("GST_MESSAGE_CLOCK_LOST: The current clock became unusable. Applications should reset the pipeline to PLAYING state after pausing.\n"); break;
-						case GST_MESSAGE_NEW_CLOCK: common::println("GST_MESSAGE_NEW_CLOCK: A new clock was selected in the pipeline.\n"); break;
-						case GST_MESSAGE_STRUCTURE_CHANGE: common::println("GST_MESSAGE_STRUCTURE_CHANGE: The structure of the pipeline changed (internal).\n"); break;
+						case GST_MESSAGE_STATE_DIRTY: common::println_loc("GST_MESSAGE_STATE_DIRTY: An element changed state in a streaming thread (deprecated)."); break;
+						case GST_MESSAGE_STEP_DONE: common::println_loc("GST_MESSAGE_STEP_DONE: A stepping operation finished."); break;
+						case GST_MESSAGE_CLOCK_PROVIDE: common::println_loc("GST_MESSAGE_CLOCK_PROVIDE: An element provides a clock. This is an internal message."); break;
+						case GST_MESSAGE_CLOCK_LOST: common::println_loc("GST_MESSAGE_CLOCK_LOST: The current clock became unusable. Applications should reset the pipeline to PLAYING state after pausing."); break;
+						case GST_MESSAGE_NEW_CLOCK: common::println_loc("GST_MESSAGE_NEW_CLOCK: A new clock was selected in the pipeline."); break;
+						case GST_MESSAGE_STRUCTURE_CHANGE: common::println_loc("GST_MESSAGE_STRUCTURE_CHANGE: The structure of the pipeline changed (internal)."); break;
 						case GST_MESSAGE_STREAM_STATUS:
-							common::println("GST_MESSAGE_STREAM_STATUS:");
+							common::print_loc("GST_MESSAGE_STREAM_STATUS:");
 							GstStreamStatusType stream_status;
 							GstElement *owner;
 							gst_message_parse_stream_status(msg, &stream_status, &owner);
 							switch (stream_status) {
-								case GST_STREAM_STATUS_TYPE_CREATE:
-									common::println("GST_STREAM_STATUS_TYPE_CREATE:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_CREATE.\n", GST_OBJECT_NAME(msg->src));
-									break;
-								case GST_STREAM_STATUS_TYPE_ENTER:
-									common::println("GST_STREAM_STATUS_TYPE_ENTER:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_ENTER.\n", GST_OBJECT_NAME(msg->src));
-									break;
-								case GST_STREAM_STATUS_TYPE_LEAVE:
-									common::println("GST_STREAM_STATUS_TYPE_LEAVE:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_LEAVE.\n", GST_OBJECT_NAME(msg->src));
-									break;
-								case GST_STREAM_STATUS_TYPE_DESTROY:
-									common::println("GST_STREAM_STATUS_TYPE_DESTROY:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_DESTROY.\n", GST_OBJECT_NAME(msg->src));
-									break;
-								case GST_STREAM_STATUS_TYPE_START:
-									common::println("GST_STREAM_STATUS_TYPE_START:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_START.\n", GST_OBJECT_NAME(msg->src));
-									break;
-								case GST_STREAM_STATUS_TYPE_PAUSE:
-									common::println("GST_STREAM_STATUS_TYPE_PAUSE:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_PAUSE.\n", GST_OBJECT_NAME(msg->src));
-									break;
-								case GST_STREAM_STATUS_TYPE_STOP:
-									common::println("GST_STREAM_STATUS_TYPE_STOP:");
-									g_print("Status of a stream of %s is GST_STREAM_STATUS_TYPE_STOP.\n", GST_OBJECT_NAME(msg->src));
-									break;
+								case GST_STREAM_STATUS_TYPE_CREATE: common::println("Status of a stream of", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_CREATE."); break;
+								case GST_STREAM_STATUS_TYPE_ENTER: common::println("Status of a stream of ", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_ENTER."); break;
+								case GST_STREAM_STATUS_TYPE_LEAVE: common::println("Status of a stream of ", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_LEAVE."); break;
+								case GST_STREAM_STATUS_TYPE_DESTROY: common::println("Status of a stream of ", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_DESTROY."); break;
+								case GST_STREAM_STATUS_TYPE_START: common::println("Status of a stream of ", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_START."); break;
+								case GST_STREAM_STATUS_TYPE_PAUSE: common::println("Status of a stream of ", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_PAUSE."); break;
+								case GST_STREAM_STATUS_TYPE_STOP: common::println("Status of a stream of ", GST_OBJECT_NAME(msg->src), " is GST_STREAM_STATUS_TYPE_STOP."); break;
 							}
 							break;
-						case GST_MESSAGE_APPLICATION: common::println("GST_MESSAGE_APPLICATION: An application-specific message.\n"); break;
-						case GST_MESSAGE_ELEMENT: common::println("GST_MESSAGE_ELEMENT: An element-specific message.\n"); break;
-						case GST_MESSAGE_SEGMENT_START: common::println("GST_MESSAGE_SEGMENT_START: Playback of a segment started (internal).\n"); break;
-						case GST_MESSAGE_SEGMENT_DONE: common::println("GST_MESSAGE_SEGMENT_DONE: Playback of a segment completed.\n"); break;
-						case GST_MESSAGE_DURATION_CHANGED: common::println("GST_MESSAGE_DURATION_CHANGED: The pipeline's duration changed. Applications can query the new duration.\n"); break;
-						case GST_MESSAGE_LATENCY: common::println("GST_MESSAGE_LATENCY: The latency of elements changed, requiring recalculation.\n"); break;
-						case GST_MESSAGE_ASYNC_START: common::println("GST_MESSAGE_ASYNC_START: An asynchronous state change started (internal).\n"); break;
-						case GST_MESSAGE_ASYNC_DONE: common::println("GST_MESSAGE_ASYNC_DONE: An asynchronous state change completed.\n"); break;
-						case GST_MESSAGE_REQUEST_STATE: common::println("GST_MESSAGE_REQUEST_STATE: An element requests a state change in the pipeline.\n"); break;
-						case GST_MESSAGE_STEP_START: common::println("GST_MESSAGE_STEP_START: A stepping operation started.\n"); break;
-						case GST_MESSAGE_QOS: common::println("GST_MESSAGE_QOS: A buffer was dropped or an element adjusted its processing for Quality of Service reasons.\n"); break;
-						case GST_MESSAGE_PROGRESS: common::println("GST_MESSAGE_PROGRESS: A progress-related message.\n"); break;
-						case GST_MESSAGE_TOC: common::println("GST_MESSAGE_TOC: A table of contents (TOC) was found or updated.\n"); break;
-						case GST_MESSAGE_RESET_TIME: common::println("GST_MESSAGE_RESET_TIME: A request to reset the pipeline's running time (internal).\n"); break;
-						case GST_MESSAGE_STREAM_START: common::println("GST_MESSAGE_STREAM_START: A new stream started. Useful in gapless playback scenarios.\n"); break;
-						case GST_MESSAGE_NEED_CONTEXT: common::println("GST_MESSAGE_NEED_CONTEXT: An element requests a specific context.\n"); break;
-						case GST_MESSAGE_HAVE_CONTEXT: common::println("GST_MESSAGE_HAVE_CONTEXT: An element created a context.\n"); break;
-						case GST_MESSAGE_EXTENDED: common::println("GST_MESSAGE_EXTENDED: Marks an extended message type. Cannot be used directly in mask-based APIs but can be checked specifically.\n"); break;
-						case GST_MESSAGE_DEVICE_ADDED: common::println("GST_MESSAGE_DEVICE_ADDED: A GstDevice was added to a GstDeviceProvider.\n"); break;
-						case GST_MESSAGE_DEVICE_REMOVED: common::println("GST_MESSAGE_DEVICE_REMOVED: A GstDevice was removed from a GstDeviceProvider.\n"); break;
-						case GST_MESSAGE_PROPERTY_NOTIFY: common::println("GST_MESSAGE_PROPERTY_NOTIFY: A GObject property was changed.\n"); break;
-						case GST_MESSAGE_STREAM_COLLECTION: common::println("GST_MESSAGE_STREAM_COLLECTION: A new GstStreamCollection is available.\n"); break;
-						case GST_MESSAGE_STREAMS_SELECTED: common::println("GST_MESSAGE_STREAMS_SELECTED: The active selection of streams has changed.\n"); break;
-						case GST_MESSAGE_REDIRECT: common::println("GST_MESSAGE_REDIRECT: A redirection to another URL was requested.\n"); break;
-						case GST_MESSAGE_DEVICE_CHANGED: common::println("GST_MESSAGE_DEVICE_CHANGED: A GstDevice was changed in a GstDeviceProvider.\n"); break;
-						case GST_MESSAGE_INSTANT_RATE_REQUEST: common::println("GST_MESSAGE_INSTANT_RATE_REQUEST: Request for an instant rate change, possibly in the past.\n"); break;
-						case GST_MESSAGE_ANY:
-							common::println("GST_MESSAGE_ANY:");
-							g_print("A mask for all messages.\n");
-							break;
+						case GST_MESSAGE_APPLICATION: common::println_loc("GST_MESSAGE_APPLICATION: An application-specific message."); break;
+						case GST_MESSAGE_ELEMENT: common::println_loc("GST_MESSAGE_ELEMENT: An element-specific message."); break;
+						case GST_MESSAGE_SEGMENT_START: common::println_loc("GST_MESSAGE_SEGMENT_START: Playback of a segment started (internal)."); break;
+						case GST_MESSAGE_SEGMENT_DONE: common::println_loc("GST_MESSAGE_SEGMENT_DONE: Playback of a segment completed."); break;
+						case GST_MESSAGE_DURATION_CHANGED: common::println_loc("GST_MESSAGE_DURATION_CHANGED: The pipeline's duration changed. Applications can query the new duration."); break;
+						case GST_MESSAGE_LATENCY: common::println_loc("GST_MESSAGE_LATENCY: The latency of elements changed, requiring recalculation."); break;
+						case GST_MESSAGE_ASYNC_START: common::println_loc("GST_MESSAGE_ASYNC_START: An asynchronous state change started (internal)."); break;
+						case GST_MESSAGE_ASYNC_DONE: common::println_loc("GST_MESSAGE_ASYNC_DONE: An asynchronous state change completed."); break;
+						case GST_MESSAGE_REQUEST_STATE: common::println_loc("GST_MESSAGE_REQUEST_STATE: An element requests a state change in the pipeline."); break;
+						case GST_MESSAGE_STEP_START: common::println_loc("GST_MESSAGE_STEP_START: A stepping operation started."); break;
+						case GST_MESSAGE_QOS: common::println_loc("GST_MESSAGE_QOS: A buffer was dropped or an element adjusted its processing for Quality of Service reasons."); break;
+						case GST_MESSAGE_PROGRESS: common::println_loc("GST_MESSAGE_PROGRESS: A progress-related message."); break;
+						case GST_MESSAGE_TOC: common::println_loc("GST_MESSAGE_TOC: A table of contents (TOC) was found or updated."); break;
+						case GST_MESSAGE_RESET_TIME: common::println_loc("GST_MESSAGE_RESET_TIME: A request to reset the pipeline's running time (internal)."); break;
+						case GST_MESSAGE_STREAM_START: common::println_loc("GST_MESSAGE_STREAM_START: A new stream started. Useful in gapless playback scenarios."); break;
+						case GST_MESSAGE_NEED_CONTEXT: common::println_loc("GST_MESSAGE_NEED_CONTEXT: An element requests a specific context."); break;
+						case GST_MESSAGE_HAVE_CONTEXT: common::println_loc("GST_MESSAGE_HAVE_CONTEXT: An element created a context."); break;
+						case GST_MESSAGE_EXTENDED: common::println_loc("GST_MESSAGE_EXTENDED: Marks an extended message type. Cannot be used directly in mask-based APIs but can be checked specifically."); break;
+						case GST_MESSAGE_DEVICE_ADDED: common::println_loc("GST_MESSAGE_DEVICE_ADDED: A GstDevice was added to a GstDeviceProvider."); break;
+						case GST_MESSAGE_DEVICE_REMOVED: common::println_loc("GST_MESSAGE_DEVICE_REMOVED: A GstDevice was removed from a GstDeviceProvider."); break;
+						case GST_MESSAGE_PROPERTY_NOTIFY: common::println_loc("GST_MESSAGE_PROPERTY_NOTIFY: A GObject property was changed."); break;
+						case GST_MESSAGE_STREAM_COLLECTION: common::println_loc("GST_MESSAGE_STREAM_COLLECTION: A new GstStreamCollection is available."); break;
+						case GST_MESSAGE_STREAMS_SELECTED: common::println_loc("GST_MESSAGE_STREAMS_SELECTED: The active selection of streams has changed."); break;
+						case GST_MESSAGE_REDIRECT: common::println_loc("GST_MESSAGE_REDIRECT: A redirection to another URL was requested."); break;
+						case GST_MESSAGE_DEVICE_CHANGED: common::println_loc("GST_MESSAGE_DEVICE_CHANGED: A GstDevice was changed in a GstDeviceProvider."); break;
+						case GST_MESSAGE_INSTANT_RATE_REQUEST: common::println_loc("GST_MESSAGE_INSTANT_RATE_REQUEST: Request for an instant rate change, possibly in the past."); break;
+						case GST_MESSAGE_ANY: common::println_loc("GST_MESSAGE_ANY: A mask for all messages."); break;
 						default: std::unreachable();
 					}
 					gst_message_unref(msg);
@@ -251,18 +224,21 @@ class StreamingImageNode : public Runner<ImageData> {
 				}
 				break;
 			case GST_FLOW_FLUSHING:
-			case GST_FLOW_CUSTOM_SUCCESS_2: break;
-			case GST_FLOW_CUSTOM_SUCCESS_1: break;
-			case GST_FLOW_CUSTOM_SUCCESS: break;
-			case GST_FLOW_NOT_LINKED: throw common::Exception("Error pushing buffer to appsrc GST_FLOW_NOT_LINKED!");
-			case GST_FLOW_EOS: throw common::Exception("Error pushing buffer to appsrc GST_FLOW_EOS!");
-			case GST_FLOW_NOT_NEGOTIATED: throw common::Exception("Error pushing buffer to appsrc GST_FLOW_NOT_NEGOTIATED!");
-			case GST_FLOW_ERROR: throw common::Exception("Error pushing buffer to appsrc GST_FLOW_ERROR!");
-			case GST_FLOW_NOT_SUPPORTED: break;
-			case GST_FLOW_CUSTOM_ERROR: break;
-			case GST_FLOW_CUSTOM_ERROR_1: break;
-			case GST_FLOW_CUSTOM_ERROR_2: break;
-			default: throw common::Exception("Error pushing buffer to appsrc!");
+				common::println_warn_loc("GST_FLOW_FLUSHING: Couldn't push buffer to appsrc. Stream ended?");
+				user_data->playing.clear();
+				break;
+			case GST_FLOW_CUSTOM_SUCCESS_2:
+			case GST_FLOW_CUSTOM_SUCCESS_1:
+			case GST_FLOW_CUSTOM_SUCCESS:
+			case GST_FLOW_NOT_LINKED:
+			case GST_FLOW_EOS:
+			case GST_FLOW_NOT_NEGOTIATED:
+			case GST_FLOW_ERROR:
+			case GST_FLOW_NOT_SUPPORTED:
+			case GST_FLOW_CUSTOM_ERROR:
+			case GST_FLOW_CUSTOM_ERROR_1:
+			case GST_FLOW_CUSTOM_ERROR_2:
+			default: common::println_critical_loc("Error pushing buffer to appsrc!, ", ret);
 		}
 	}
 };
