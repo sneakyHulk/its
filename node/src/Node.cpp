92caeddf68be6f356118a77e8ae0dc56583c5bfb
Lukas Heyn <lukas.heyn@gmail.com>
2025-01-15 00:29:12 +0100

    finishing touches

--- node/src/Node.cpp
+++
@@ -1,8 +0,0 @@
-#include "Node.h"
-
-#include <iostream>
-
-/**
- * @brief Outputs the name of the current node type.
- */
-void Node::name() { std::cout << typeid(*this).name() << std::endl; }
--- node/src/ProcessorSynchronousPair.cpp
+++
@@ -1,1 +0,0 @@
-#include "ProcessorSynchronousPair.h"
